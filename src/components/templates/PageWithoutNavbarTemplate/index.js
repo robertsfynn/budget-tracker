eca833d186423b0905a4324f69b213fff197b17c
Fynn Roberts <robertsfynn@gmail.com>
2019-07-09 14:14:50 +0200

    removed padding again

--- src/components/templates/PageWithoutNavbarTemplate/index.js
+++ src/components/templates/PageWithoutNavbarTemplate/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const StyledMain = styled.main`
-  padding-bottom: 2rem;
-`;
-
-const PageTemplate = ({ children }) => <StyledMain>{children}</StyledMain>;
+const PageTemplate = ({ children }) => <main>{children}</main>;
 
 export default PageTemplate;
